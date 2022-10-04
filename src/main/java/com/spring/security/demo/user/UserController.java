15e69b95468d29bde80b2aa98b640d288a308324
Tony Kanyamuka <tonykanyamuka@gmail.com>
2022-10-04 22:46:40 +0200

    implemented admin or executive roles can update user roles policy

--- src/main/java/com/spring/security/demo/user/UserController.java
+++ src/main/java/com/spring/security/demo/user/UserController.java
@@ -46,6 +46,7 @@ public class UserController {
     }
 
     @PutMapping("update-role/{userName}/{role}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_EXECUTIVE')")
     public ResponseEntity<ApiResponse> updateUserRole(@PathVariable("userName") String userName,
                                                       @PathVariable("role") String role,
                                                       Authentication currentUser){
